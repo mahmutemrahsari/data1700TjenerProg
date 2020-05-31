fe30409d8ae8414e75e67ea63936b8271a892786
mahmutemrahsari <mahmutemrahsari@gmail.com>
2020-05-31 16:26:45 +0200

    Første tjener-prog

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String heiVerden(String navn){
+        return "Hei verden" + navn;
+    }
+}
